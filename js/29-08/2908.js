823d4326c370c85e6fe80940bc8724e2e2a0bab7
e-arauj0 <oliveiraemily322@gmail.com>
2024-08-29 21:29:36 -0300

    exercicios 29-08 (termo This) coment

--- js/29-08/2908.js
+++ js/29-08/2908.js
@@ -4,22 +4,22 @@ const alunos = [{ nome: 'Afonso', aprovado: 'sim' },
 var alunosAprovadors = ''
 
 
-function exibir(){
-    alunos.forEach(mula =>{
+function exibir(){ //função para analisar a array e adicionar a String alunosAprovadors
+    alunos.forEach(mula =>{ // para cada objeto dentro da array, irá se chamar 'mula' assim podendo fazer uma analise individual a cada objeto
         
-        if (mula.aprovado === 'sim'){
+        if (mula.aprovado === 'sim'){ //Se 'sim' ele vai exibir o nome
             this.alunosAprovadors += mula.aluno + '<br/>'
         }
     })
     
-    document.getElementById('exibirBox').innerHTML = this.alunosAprovadors
+    document.getElementById('exibirBox').innerHTML = this.alunosAprovadors //comando para exibir a String alunosAprovadors
 }
-function atualizar(){
-    a= document.getElementById('valorum').value 
-    b= document.getElementById('valordois').value
+function atualizar(){ //Função para atualizar a lista com novos valores
+    a= document.getElementById('valorum').value //escuta o valor de 'a'
+    b= document.getElementById('valordois').value // escuta o valor de 'b'
 
-    alunos.push({aluno: a, aprovado: b})
+    alunos.push({aluno: a, aprovado: b}) //insere os valores 'a' e 'b' na listal 'alunos'
 
-    this.exibir()
+    this.exibir()//chama a função 'exibir' para reavaliar a lista 'alunos' com os novos valores
 }
 
