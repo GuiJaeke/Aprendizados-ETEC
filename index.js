10a8fc62e2feb14d31caa8feb30bf46a3f785e06
GuilhermeJaeke <suporte05@montanna.com.br>
2024-11-08 15:41:13 -0300

    criando tela de login e sign e todo o backend por tras sem uso de cookies

--- index.js
+++ index.js
@@ -8,74 +8,114 @@ const { engine } = require("express-handlebars");
 
 //###CONEXÃO BANCO DE DADOS###
 const conexaoComBanco = new Sequelize("deposito", "root", "", {
-    host: "localhost",
-    dialect: "mysql",
-  });
-  
-  
-  const ferramenta = conexaoComBanco.define("ferramenta", {
-    nome_fer: {
-      type: Sequelize.STRING,//VARCHAR
-      allowNull: false
-    }
-  });
-  const user = conexaoComBanco.define("user", {
-    nome_user: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    password: {
-      type: Sequelize.STRING,
-      allowNull: false
-    }
-  });
-  const aluguel = conexaoComBanco.define("aluguel", {
-    userId: {
-      type: Sequelize.INTEGER,
-      references: { model: user, key: 'id' }
-    },
-    ferramentaId: {
-      type: Sequelize.INTEGER,
-      references: { model: ferramenta, key: 'id' }
-    }
-  });
-  async function createDB() {
-    await user.sync();
-    await ferramenta.sync();
-    await aluguel.sync();
+  host: "localhost",
+  dialect: "mysql",
+});
+
+
+const ferramenta = conexaoComBanco.define("ferramenta", {
+  nome_fer: {
+    type: Sequelize.STRING,//VARCHAR
+    allowNull: false
   }
-  async function createData() {
-    await createDB()
+});
+const User = conexaoComBanco.define("User", {
+  nome_user: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
-  createData()
+});
+const aluguel = conexaoComBanco.define("aluguel", {
+  userId: {
+    type: Sequelize.INTEGER,
+    references: { model: User, key: 'id' }
+  },
+  ferramentaId: {
+    type: Sequelize.INTEGER,
+    references: { model: ferramenta, key: 'id' }
+  }
+});
+async function createDB(force) {
+  await User.sync(force);
+  await ferramenta.sync(force);
+  await aluguel.sync(force);
+}
+async function createData() {
+  await createDB({force: false})
+}
+createData()
 //### FIM CONEXÃO BANCO DE DADOS###
 
 app.use(
   express.urlencoded({
-          extended: true
-      })
+    extended: true
+  })
 )
 
 //###ROTAS:
-app.get("/",function (req, res) {
-    console.log('aqui kraio');
-    res.render('home');
+app.get('/', function(req, res) {
+  res.redirect('/login')
+})
+
+app.get("/home", async function (req, res) {
+  const ferramentas = await ferramenta.findAll({raw: true})
+  res.render('home', {ferramentas: ferramentas});
 });
 
-app.get("/teste", async function (req, res) {
-  res.send("Hello Teste");
-});
 
-
-app.get("/cad/form",async function (req, res) {
+app.get("/cad/form", async function (req, res) {
   res.render('form');
 });
 
+app.get('/sign', async function sign(req, res) {
+  let login = true
+  res.render('sign', {login})
+})
+app.post('/sign', async function signPost(req, res) {
+  const { nome_user, password } = req.body
+  await User.create({nome_user, password})
+  res.redirect('/login')
+
+})
+
+app.get('/login', async function login(req, res) {
+  let login = true
+  res.render('login', {login})
+  
+})
+app.post('/login', async function loginPost(req, res) {
+  const {userLogin, password} = req.body
+  console.log(userLogin);
+  
+
+  const usuario = await User.findOne({where: { nome_user: userLogin }, raw: true})
+
+  console.log(usuario);
+  if (!usuario) {  
+    return res.redirect('/login')
+  }
+  if (usuario.password == password) {
+    console.log('here1');
+    return res.redirect('/home')
+  } else {
+    console.log('here2');
+    
+    return res.redirect('/login')
+  }
+
+})
+
 app.post("/cad/ferr", async function (req, res) {
-    const ferr = req.body.fer
-    console.log(ferr);
-    await ferramenta.create({nome_fer: ferr})
-    res.redirect('/home') //renderizando a pagina form.handlebars
+  const ferr = req.body.fer
+  const quantidade = req.body.quantidade
+  for(let i = 0; i < quantidade; i++) {
+    await ferramenta.create({ nome_fer: ferr })
+  }
+  res.redirect('/') //renderizando a pagina form.handlebars
 });
 //###FIM ROTAS
 
